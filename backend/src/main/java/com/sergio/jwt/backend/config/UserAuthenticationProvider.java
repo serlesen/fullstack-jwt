Estoutic/cloth_backend

Implement the JWT authentication with a React frontend and a Spring Boot backend

backend/src/main/java/com/sergio/jwt/backend/config/UserAuthenticationProvider.java
@@ -1,0 +1,61 @@
+package com.sergio.jwt.backend.config;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.sergio.jwt.backend.dtos.UserDto;
+import com.sergio.jwt.backend.repositories.UserRepository;
+import com.sergio.jwt.backend.services.UserService;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Base64;
+import java.util.Collections;
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Component
+public class UserAuthenticationProvider {
+
+    @Value("${security.jwt.token.secret-key:secret-key}")
+    private String secretKey;
+
+    private final UserService userService;
+
+    @PostConstruct
+    protected void init() {
+        // this is to avoid having the raw secret key available in the JVM
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    public String createToken(String login) {
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + 3600000); // 1 hour
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        return JWT.create()
+                .withIssuer(login)
+                .withIssuedAt(now)
+                .withExpiresAt(validity)
+                .sign(algorithm);
+    }
+
+    public Authentication validateToken(String token) {
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+        JWTVerifier verifier = JWT.require(algorithm)
+                .build();
+
+        DecodedJWT decoded = verifier.verify(token);
+
+        UserDto user = userService.findByLogin(decoded.getIssuer());
+
+        return new UsernamePasswordAuthenticationToken(user, null, Collections.emptyList());
+    }
+
+}

serlesen/fullstack-jwt

backend/src/main/java/com/sergio/jwt/backend/config/UserAuthenticationProvider.java
@@ -1,0 +1,61 @@
+package com.sergio.jwt.backend.config;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.sergio.jwt.backend.dtos.UserDto;
+import com.sergio.jwt.backend.repositories.UserRepository;
+import com.sergio.jwt.backend.services.UserService;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Base64;
+import java.util.Collections;
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Component
+public class UserAuthenticationProvider {
+
+    @Value("${security.jwt.token.secret-key:secret-key}")
+    private String secretKey;
+
+    private final UserService userService;
+
+    @PostConstruct
+    protected void init() {
+        // this is to avoid having the raw secret key available in the JVM
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    public String createToken(String login) {
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + 3600000); // 1 hour
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        return JWT.create()
+                .withSubject(login)
+                .withIssuedAt(now)
+                .withExpiresAt(validity)
+                .sign(algorithm);
+    }
+
+    public Authentication validateToken(String token) {
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+        JWTVerifier verifier = JWT.require(algorithm)
+                .build();
+
+        DecodedJWT decoded = verifier.verify(token);
+
+        UserDto user = userService.findByLogin(decoded.getSubject());
+
+        return new UsernamePasswordAuthenticationToken(user, null, Collections.emptyList());
+    }
+
+}